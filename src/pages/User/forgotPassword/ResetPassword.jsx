dcad14bdc5110c534cf8417437707af834941608
haithembenhalima <haithembenhalima12@gmail.com>
2024-09-19 20:17:18 +0100

    adding the 404 component

--- src/pages/User/forgotPassword/ResetPassword.jsx
+++ src/pages/User/forgotPassword/ResetPassword.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Navbar from "../../../components/navbar/Navbar";
 import Footer from "../../../components/footer/Footer";
+import NotFound from "../../../components/notFound/NotFound";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { FloatLabel } from "primereact/floatlabel";
 import { Toast } from "primereact/toast";
-import {resetAction} from '../../../redux/resetPassword/actions'
+import { resetAction } from "../../../redux/resetPassword/actions";
 
 function ResetPassword() {
-  
   const toast = useRef(null);
   const [password, setPassword] = useState(null);
   const [email, setEmail] = useState(null);
@@ -21,19 +21,18 @@ function ResetPassword() {
     (state) => state.resetPassword
   );
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     const resetData = {
       email,
       password,
-    }
+    };
     try {
       const result = await dispatch(resetAction(resetData));
 
       if (!resetAction.fulfilled.match(result)) {
         console.log(result);
-        
+
         toast.current.show({
           severity: "error",
           summary: "حدث خطأ",
@@ -41,14 +40,12 @@ function ResetPassword() {
           life: 6000,
         });
       } else if (resetAction.fulfilled.match(result)) {
-  
         toast.current.show({
           severity: "success",
           summary: " تمت العميلة بنجاح",
           detail: "تم تغيير كبمة السر بنجاح",
           life: 6000,
         });
-  
       }
     } catch (error) {
       console.log(error);
@@ -58,20 +55,16 @@ function ResetPassword() {
         detail: "يرجى المحاولة",
         life: 6000,
       });
-      
     }
-
   };
 
-  return (
-    localStorage.getItem('resetPasswordStatus')? 
+  return localStorage.getItem("resetPasswordStatus") ? (
     <div className="container">
       <Navbar />
       <div className="forgot-form" style={forgotFormStyle}>
         <h3>أدخل كلمة السر الجديدة</h3>
         <form onSubmit={handleSubmit} style={formStyle}>
-
-        <FloatLabel style={{ marginTop: "35px" }}>
+          <FloatLabel style={{ marginTop: "35px" }}>
             <InputText
               id="email"
               value={email}
@@ -83,7 +76,7 @@ function ResetPassword() {
             </label>
           </FloatLabel>
 
-        <FloatLabel style={{ marginTop: "35px" }}>
+          <FloatLabel style={{ marginTop: "35px" }}>
             <InputText
               id="password"
               type="password"
@@ -91,7 +84,10 @@ function ResetPassword() {
               onChange={(e) => setPassword(e.target.value)}
               style={inputStyle}
             />
-            <label htmlFor="password" style={{ marginBottom: 5, marginLeft: 250 }}>
+            <label
+              htmlFor="password"
+              style={{ marginBottom: 5, marginLeft: 250 }}
+            >
               كلمة السر الجديدة
             </label>
           </FloatLabel>
@@ -107,15 +103,10 @@ function ResetPassword() {
       <Toast ref={toast} />
       <Footer />
     </div>
-
-   // if is not confirmed the reset password:  404 Not Found
-    : <div className="container">
-      <Navbar />
-      <div className="forgot-form" style={forgotFormStyle}>
-        <h3>لم تقم ب��رسال رابط التغيير للبريد ال��لكتروني</h3>
-        <Link to="/forgot-password">��عادة التوجيه ��لى ��فحة التغيير</Link>
-      </div>
-      <Footer />
+  ) : (
+    // if is not confirmed the reset password:  404 Not Found
+    <div className="container">
+      <NotFound />
     </div>
   );
 }
